4a9fde38821212fa639a5dcda0872cb27532a81a
GeoHaber <11169518+GeoHaber@users.noreply.github.com>
2023-01-26 11:18:08 -0500

    Update ChatGPT_voice.py
    Did some reformat the code

--- ChatGPT_voice.py
+++ ChatGPT_voice.py
@@ -1,40 +1,64 @@
 #pip install SpeechRecognition openai gtts playsound
 
-from pydub import AudioSegment
-from pydub.playback import play
+from gtts import gTTS
 import speech_recognition as sr
 import openai
-import gtts
-from playsound import playsound
+from pydub import AudioSegment
+from pydub.playback import play
 
-# Use the API key
-#openai.api_key = "YOUR_API_KEY"
+def voice_interface():
+    # Initialize the speech recognizer and microphone
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
 
-# initialize the recognizer
-r = sr.Recognizer()
+    ## XXX:  Set the OpenAI API key
+    openai.api_key = "YOUR_API_KEY"
+    openai.api_key = "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"
 
-# read the audio file
-with sr.Microphone() as source:
-    print("Speak: ")
-    audio = r.listen(source)
+    while True:
+        try:
+            ## XXX:  Prompt the user for voice input
+            print("Please speak a command:")
+            with microphone as source:
+                audio = recognizer.listen(source)
 
-# recognize the speech
-text = r.recognize_google(audio)
+            ## XXX:  Convert the audio to text
+            command = recognizer.recognize_google(audio, language="en-US")
+            print("Command: ", command)
 
-# Send a request to the API
-response = openai.Completion.create(
-    engine="text-davinci-002",
-    prompt=text,
-    max_tokens=2048
-)
+            if command == "Ok goodbye":
+                print("Goodbye!")
+                break
 
-# Use gTTS to convert text to speech
-tts = gtts.gTTS(response["choices"][0]["text"])
+            ## XXX:  Generate the response from ChatGPT
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt='You said: '+ command,
+                max_tokens=2048,
+                n = 1,
+                stop=None,
+                temperature=0.5
+            )
 
-# Save the speech to a file
-tts.save("response.mp3")
+            response = response.choices[0].text
+            print("Response: ", response)
+            text_to_speech ( response )
 
-audio = AudioSegment.from_file("response.mp3")
-play(audio)
-# Play the audio file
-playsound("response.mp3")
+        except sr.UnknownValueError:
+            print("Sorry, I did not understand what you said.")
+        except sr.RequestError as e:
+            print("Error while requesting results; {0}".format(e))
+        except Exception as e:
+            print("Error: {0}".format(e))
+#function to convert the text to speech
+def text_to_speech(text):
+    tts = gTTS(text=text, lang='en')
+    tts.save("response.mp3")
+
+    audio = AudioSegment.from_file("response.mp3")
+    play(audio)
+    # Play the audio file
+#    playsound("response.mp3")
+
+if __name__ == '__main__':
+    voice_interface()
