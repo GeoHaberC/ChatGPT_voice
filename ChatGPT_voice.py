315157e936428026998579337a9af753d2de8bc0
GeoHaber <11169518+GeoHaber@users.noreply.github.com>
2023-02-22 23:37:34 -0500

    Update ChatGPT_voice.py
    various bug fixes and enhancements

--- ChatGPT_voice.py
+++ ChatGPT_voice.py
@@ -1,63 +1,129 @@
-#pip install SpeechRecognition openai gtts playsound
-
-from gtts import gTTS
-import speech_recognition as sr
+import os
+import sys
+import time
 import openai
-from pydub import AudioSegment
+import pyaudio
+import logging
+import speech_recognition as sr
+
+from gtts			import gTTS
+from functools		import wraps
+from pydub			import AudioSegment
 from pydub.playback import play
 
-def voice_interface():
-    # Initialize the speech recognizer and microphone
-    recognizer = sr.Recognizer()
-    microphone = sr.Microphone()
+how_can_I_help = 'How can I help'
 
-    ## XXX:  Set the OpenAI API key
-    openai.api_key = "YOUR_API_KEY"
+def handle_exception(func):
+	"""Decorator to handle exceptions."""
+	@wraps(func)
+	def wrapper(*args, **kwargs):
+		try:
+			return func(*args, **kwargs)
+		except Exception as e:
+			logging.error("Error: %s", e, exc_info=True)
+			sys.exit(1)
+	return wrapper
 
-    while True:
-        try:
-            ## XXX:  Prompt the user for voice input
-            print("Please speak a command:")
-            with microphone as source:
-                audio = recognizer.listen(source)
+@handle_exception
+def check_audio_devices():
+	"""Check if the audio input and output devices are properly configured."""
+	p = pyaudio.PyAudio()
+	input_device  = p.get_default_input_device_info()
+	output_device = p.get_default_output_device_info()
 
-            ## XXX:  Convert the audio to text
-            command = recognizer.recognize_google(audio, language="en-US")
-            print("Command: ", command)
+	if input_device is None:
+		logging.error("No audio input device found.")
+		sys.exit(1)
 
-            if command == "Ok goodbye":
-                print("Goodbye!")
-                break
+	if output_device is None:
+		logging.error("No audio output device found.")
+		sys.exit(1)
 
-            ## XXX:  Generate the response from ChatGPT
-            response = openai.Completion.create(
-                engine="text-davinci-002",
-                prompt='You said: '+ command,
-                max_tokens=2048,
-                n = 1,
-                stop=None,
-                temperature=0.5
-            )
+	logging.info("Audio input device: %s", input_device['name'])
+	logging.info("Audio output device: %s", output_device['name'])
 
-            response = response.choices[0].text
-            print("Response: ", response)
-            text_to_speech ( response )
+@handle_exception
+def voice_ChatGPT():
+	"""The main function for the voice interface."""
 
-        except sr.UnknownValueError:
-            print("Sorry, I did not understand what you said.")
-        except sr.RequestError as e:
-            print("Error while requesting results; {0}".format(e))
-        except Exception as e:
-            print("Error: {0}".format(e))
-#function to convert the text to speech
+	log_file = f"_{sys._getframe().f_code.co_name}_{time.strftime('%Y%m%d-%H%M%S')}.log"
+
+	logging.basicConfig(filename=log_file,
+						level=logging.DEBUG,
+						format='%(asctime)s %(levelname)s %(message)s',
+						datefmt='%Y-%m-%d %H:%M:%S')
+
+	# Set logging level to only display INFO messages inside the loop
+	logging.getLogger().setLevel(logging.INFO)
+
+	check_audio_devices()
+
+	# Initialize the speech recognizer and microphone
+	recognizer = sr.Recognizer()
+	microphone = sr.Microphone()
+	openai.api_key = os.getenv("OPENAI_API_KEY")
+	if openai.api_key is None:
+		logging.error("API key not found.")
+		return
+
+	while True:
+		try:
+			# Prompt the user for voice input
+			text_to_speech (how_can_I_help)
+			logging.info   (how_can_I_help)
+			with microphone as source:
+				audio = recognizer.listen(source)
+			# Convert the audio to text
+			command = recognizer.recognize_google(audio, language="en-US")
+			logging.info("Command: %s", command)
+
+			if command.lower() in ["goodbye", "exit", "end program" ]:
+				text_to_speech ("Are you sure? to exit say yes")
+				with microphone as source:
+					audio = recognizer.listen(source)
+				if recognizer.recognize_google(audio, language="en-US").lower() == 'yes' :
+					logging.info("Goodbye!")
+					break
+
+			# Generate the response from OpenAI
+			response = openai.Completion.create(
+				engine="text-davinci-002",
+				prompt='You said: ' + command,
+				max_tokens=2048,
+				n=1,
+				stop=None,
+				temperature=0.5
+			)
+
+			response = response.choices[0].text
+			logging.info("Response: %s", response)
+			text_to_speech(response)
+
+		except sr.UnknownValueError:
+			logging.error("Sorry, I did not understand what you said.")
+		except sr.RequestError as e:
+			logging.error("Error while requesting results: %s", e)
+		finally :
+			# Set logging level back to original level outside the loop
+			logging.getLogger().setLevel(logging.DEBUG)
+
+	return log_file
+
+@handle_exception
 def text_to_speech(text):
-    tts = gTTS(text=text, lang='en')
-    tts.save("response.mp3")
+	"""Convert the text to speech."""
+	tts = gTTS(text=text, lang='en')
+	tts.save("tmp.mp3")
 
-    audio = AudioSegment.from_file("response.mp3")
-    play(audio)
-    # Play the audio file
-#    playsound("response.mp3")
+	audio = AudioSegment.from_file("tmp.mp3")
+	play(audio)
+
 
 if __name__ == '__main__':
-    voice_interface()
+	log_file = voice_ChatGPT()
+
+	if os.path.exists(log_file) :
+		os.remove(log_file)
+		pass
+	else:
+		print(f"{log_file} file does not exist")
